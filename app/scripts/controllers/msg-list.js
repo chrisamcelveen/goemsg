28865b23bdd788d1203111570295037170368fe3
Christopher A. McElveen <chris@chrisamcelveen.com>
2015-02-28 15:52:56 -0500

    styled the message list.

---
+++ app/scripts/controllers/msg-list.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geomsgApp.controller:MsgListCtrl
+ * @description
+ * # MsgListCtrl
+ * Controller of the geomsgApp
+ */
+angular.module('geomsgApp')
+  .controller('MsgListCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
