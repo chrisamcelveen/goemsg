4864478b48df38a75140aa413e9244abf4fd6341
Michael Brash <mbrash123@gmail.com>
2015-02-28 15:11:46 -0500

    API folder added with users.json

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -1,1 +1,26 @@
-//
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geomsgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the geomsgApp
+ */
+  app.controller('UserCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.users = []
+
+    var getUsers = function() {
+	    $http.get("/api/users.json").
+	    	success(function(data){
+	    		$scope.users = data
+	    		console.log(data)
+	    	}).error(function(data){
+	    		console.log("error")
+	    	})
+    }
+
+    getUsers()
+
+
+  }]);
